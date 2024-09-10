719350bdf829adb7693d63c9816b22a4a136525c
Dinidu21 <dinidusachinth3@gmail.com>
2024-09-10 15:09:28 +0530

    Tribonacci Seq Memoization Time: O(n)

--- src/ConceptPractice/dp/TribonacciMemoization.java
+++ src/ConceptPractice/dp/TribonacciMemoization.java
@@ -1,19 +1,29 @@
 package ConceptPractice.dp;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class TribonacciMemoization {
-    //Bruteforce method
-
+    //Dynamic Programming applied
 
     public static int trib(int n){
+        return trib(n,new HashMap<>());
+    }
+
+    public static int trib(int n, HashMap<Integer,Integer> memo){
         if(n == 0 || n == 1){
             return 0;
         }
         if(n == 2){
             return 1;
         }
-        return trib(n - 1) +trib( n - 2) + trib( n - 3);
+        if(memo.containsKey(n)){
+            return  memo.get(n);
+        }
+
+        int result =  trib(n - 1 ,memo) +trib( n - 2,memo) + trib( n - 3,memo);
+        memo.put(n,result);
+        return result;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
