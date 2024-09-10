cca9be426c45cbcbdd280e27b450608983170922
Dinidu21 <dinidusachinth3@gmail.com>
2024-09-10 14:51:12 +0530

    Tribonacci Bruteforce method Time: O(3^n)

---
+++ src/ConceptPractice/dp/TribonacciMemoization.java
@@ -0,0 +1,25 @@
+package ConceptPractice.dp;
+
+import java.util.Scanner;
+
+public class TribonacciMemoization {
+    //Bruteforce method
+
+
+    public static int trib(int n){
+        if(n == 0 || n == 1){
+            return 0;
+        }
+        if(n == 2){
+            return 1;
+        }
+        return trib(n - 1) +trib( n - 2) + trib( n - 3);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Number : ");
+        int num = sc.nextInt();
+        int result = trib(num);
+        System.out.println("Result : "+result);
+    }
+}
