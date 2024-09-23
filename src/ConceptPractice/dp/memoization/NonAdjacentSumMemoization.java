0cea0467bec5ad4bea2e622f0a21623449b0d927
Dinidu Sachintha <dinidusachintha3@gmail.com>
2024-09-23 14:11:03 +0530

    added files to memoization

---
+++ src/ConceptPractice/dp/memoization/NonAdjacentSumMemoization.java
@@ -0,0 +1,48 @@
+package ConceptPractice.dp.memoization;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class NonAdjacentSumMemoization {
+        //Dynamic Programming Approach
+        private static int nonAdjecentSum(List<Integer> nums) {
+            return nonAdjecentSum(nums,0,new HashMap<>());
+        }
+        private static int nonAdjecentSum(List<Integer> nums,int i,HashMap<Integer,Integer>memo) {
+            if(i >= nums.size()){
+                return 0; //if is i more than array size
+            }
+            if(memo.containsKey(i)){
+                return memo.get(i);
+            }
+            int result = Math.max(nums.get(i)+nonAdjecentSum(nums,i + 2,memo),
+                    nonAdjecentSum(nums,i + 1,memo));
+            memo.put(i,result);
+            return result;
+
+        }
+
+        public static void main(String[] args) {
+            List<Integer> nums = Arrays.asList(2,4,5,12,7,12,5,6,8,2,9,2,9,2,87,3,8,4,
+                    8,2,9,5,4,8,5,8,8,51,8,9,5,5,0,9,9,0,2,4,5,12,7,
+                    12,5,6,8,2,9,2,9,2,87,3,8,4,8,2,9,5,4,8,5,8,8,51,
+                    8,9,5,5,0,2,4,5,12,7,12,5,6,8,2,9,2,9,2,87,3,8,4
+                    ,8,2,9,5,4,8,5,8,8,51,8,9,5,5,0,2,4,5,12,7,12,5,
+                    6,8,2,9,2,9,2,87,3,8,4,8,2,9,5,4,8,5,8,8,51,8,9,5
+                    ,5,0,2,4,5,12,7,12,5,6,8,2,9,2,9,2,87,3,8,4,8,5,
+                    2,9,5,4,8,5,8,8,51,8,9,5,5,0,2,4,5,12,7,12,5,6
+                    ,8,2,9,2,9,2,87,3,8,4,8,2,9,5,4,8,5,8,8,51,8,5,
+                    9,5,5,0,2,4,5,12,7,12,5,6,8,2,9,2,9,2,87,3,8,4,8
+                    ,2,9,5,4,8,5,8,8,51,8,9,5,5,0,2,4,5,12,7,12,5,6,8,2,9,
+                    2,9,2,87,3,8,4,8,2,9,5,4,8,5,8,8,51,8,9,5,5,0,2,4,5,12,7,
+                    12,5,6,8,2,9,2,9,2,87,3,8,4,8,2,9,5,4,8,5,8,8,51,8,9,5,5,0,2
+                    ,4,5,12,7,12,5,6,8,2,9,2,9,2,87,3,8,4,8,2,9,5,4,8,5,8,8,51,8,9,5,5,0);
+
+            System.out.println("arrays Size : "+nums.size());
+            int result = nonAdjecentSum(nums);
+            System.out.println("Non Adjacent Sum is : " + result);
+        }
+    }
+
+
---
+++ src/ConceptPractice/dp/memoization/SumPossibleMemoization.java
@@ -0,0 +1,58 @@
+package ConceptPractice.dp.memoization;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+public class SumPossibleMemoization {
+    //Dynamic Programming Approach
+    public static boolean sumPossible(int amount , List <Integer> arr) {
+        return sumPossible(amount,arr,new HashMap<>());
+
+    }
+    public static boolean sumPossible(int amount , List <Integer> arr, HashMap <Integer,Boolean> memo) {
+        if (amount == 0){
+            return true;
+        }
+
+        if(amount < 0){
+            return false;
+        }
+        if(memo.containsKey(amount)){
+            return memo.get(amount);
+        }
+
+        for(int num : arr){
+            int subAmount = amount - num;
+            if(sumPossible(subAmount,arr,memo)){
+                memo.put(subAmount,true);
+                return true;
+            }
+        }
+        memo.put(amount,false);
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> arr = new ArrayList<>();
+
+        System.out.print("Enter list of space-separated integers: ");
+        String n = sc.nextLine();
+
+        String[] nums = n.split(" ");
+
+        for (String num : nums) {
+            arr.add(Integer.parseInt(num));
+        }
+
+        System.out.println("List: " + arr);
+
+        System.out.print("Enter the target sum: ");
+        int targetSum = sc.nextInt();
+
+        boolean value = sumPossible(targetSum,arr);
+        System.out.println("Is sum possible: " + value);
+    }
+}
